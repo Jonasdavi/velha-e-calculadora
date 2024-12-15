3b8e2558be10eec4d5f69178f0964064f1be6d87
Jonasdavi <jonas.davi@escolar.ifrn.edu.br>
2024-12-14 21:07:40 -0300

    atualizando tui

--- jogo-da-velha/src/App.java
+++ jogo-da-velha/src/App.java
@@ -1,9 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Tabuleiro t= new Tabuleiro(new Jogador('X'), new Jogador('O'));
+        TUI tui= new TUI();
         while(t.getNumJogadas()<9){
             if(t.jogar()){
                 t.exibirTabuleiro();
+                tui.desenheTabuleiro(t.getTabuleiro());
                 System.out.println();
             }
 
--- jogo-da-velha/src/TUI.java
+++ jogo-da-velha/src/TUI.java
@@ -14,10 +14,40 @@ public class TUI implements TabUI{
         //desenhando a linha 0 e coluna 0 do tabuleiro:
         
         if(tab[0][0]=='X'){ //se a posicao tab[0][0] estiver com X:
+            /*entao tenho que desenhar isso da linha e coluna 0 a 4 da tui desta forma:
+
+                0  1  2  3  4  5 
+            0   X  -  -  -  X  #  
+            1   -  X  -  X  -  #  
+            2   -  -  X  -  -  #  
+            3   -  X  -  X  -  # 
+            4   X  -  -  -  X  #  
+            5   #  #  #  #  #  #      
+            
+            */
+            // da linha e coluna 0 a 4, toda vez que a linha e coluna são iguais, aquela posição é o caractere 'X':
+            for(int i=0 ; i<=4 ; i++){
+                tui[i][i]= 'X';
+            }
+
+            //na outra diagonal do 'X':
+            //linha se inicia na 0, coluna se inicia na 4, toda vez que a linha almenta 1, a coluna diminui 1, até chegar na linha 4 e coluna 0:
+            for(int i=0; i<=4; i++){
+                tui[i][4-i]= 'X';
+            }
+            
 
         }
 
     
+
+        //depois ajustar todas as posições, exibir toda o desenho:
+        for(int l=0 ; l<tui.length ; l++){
+            for(int c=0 ; c<tui.length ; c++){
+                System.out.print(tui[l][c]);
+            }
+            System.out.println();
+        }
     }
 
     public void reiniciarTUI(){
